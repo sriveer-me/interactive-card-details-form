9eca07aac3e5e61a64c107aad7b47beeb3082670
sriveer-neerukonda <sriveer.neerukonda@hotmail.com>
2024-01-15 07:07:15 +0530

    feat: Added gradient-show and form-show elements
    The elements gradient-show and form-show are used to display the gradient image and form respectively.
    
    Added a function computeGradientShowAndFormShowWidths which when called resizes the two newly added elements
    
    The newly added function computeGradientShowAndFormShowWidths is called on DOMContentLoaded and whenever the window is resized, added callbacks to that effect.
    
    Added a new file AbstractConstants which holds the scss constants large and medium used in media queries for usage in JS.
    
    Basic styling added on main and gradient-show.

---
+++ src/AbstractConstants.ts
@@ -0,0 +1,5 @@
+//This value is taken from _abstract.scss 
+//Unexpected behavior may arise if these two files are not manually in sync
+//todo - look for a way to automatically synchronize AbstractConstants.ts and _abstract.scss
+export const largeBreakpoint = 1200; //1200px - _abstract.scss 
+export const mediumBreakpoint = 769; //769px - abstract.scss
